0f53bf704c954b8f0fd4cc9f05737f40c16c5df0
furkan harmancı <rocklee34@hotmail.com>
2023-10-30 22:39:04 +0300

    feat: getNote class added to appModule :sparkles:

--- app/src/main/java/com/example/note_app/di/AppModule.kt
+++ app/src/main/java/com/example/note_app/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.example.note_app.feature_note.data.repository.NoteRepositoryImpl
 import com.example.note_app.feature_note.domain.repository.NoteRepository
 import com.example.note_app.feature_note.domain.use_case.AddNote
 import com.example.note_app.feature_note.domain.use_case.DeleteNote
+import com.example.note_app.feature_note.domain.use_case.GetNote
 import com.example.note_app.feature_note.domain.use_case.GetNotes
 import com.example.note_app.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -42,7 +43,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
